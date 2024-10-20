a0e73ba517b17aaed90ba646cdad6be7b5c85162
ElliotBruhl <166664797+ElliotBruhl@users.noreply.github.com>
2024-10-20 15:56:36 -0400

    Update counterType.hpp
    added pre/post conditions

--- counterType.hpp
+++ counterType.hpp
@@ -3,11 +3,11 @@ class CounterType {
   private:
     int counter;
   public:
-    CounterType(); //default constructor
-    CounterType(int value); //constructor with 1 arg
-    void setCounter(int value); //set counter to value
-    void resetCounter(); //reset counter to 0
-    void incrementCounter(); //increment counter by 1
-    void decrementCounter(); //decrement counter by 1
-    int getCount(); //return counter
+    CounterType(); //pre: enough memory. post: object is created
+    CounterType(int value); //pre: enough memory. post: object is created with initial value
+    void setCounter(int value); //pre: value is in domain (0 - MAX). post: counter is value
+    void resetCounter(); //pre: none. post: counter is 0
+    void incrementCounter(); //pre: counter is not MAX. post: counter is one greater
+    void decrementCounter(); //pre: counter is not 0. post: counter is one less
+    int getCount(); //pre: none. post: none.
 };
